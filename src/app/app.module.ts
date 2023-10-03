e0f0e039fd70036d7e18ca707e70047c09ff1c5a
Benjamin Tietz <benjamin0tietz@gmail.com>
2023-10-03 22:23:26 +0200

    started to work at startscreen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { StartscreenComponent } from './startscreen/startscreen.component';
+import { AboutComponent } from './about/about.component';
+import { ContactMeComponent } from './contact-me/contact-me.component';
+import { FixedItemsComponent } from './fixed-items/fixed-items.component';
+import { FooterComponent } from './footer/footer.component';
+import { ImprintComponent } from './imprint/imprint.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    StartscreenComponent,
+    AboutComponent,
+    ContactMeComponent,
+    FixedItemsComponent,
+    FooterComponent,
+    ImprintComponent,
+    PortfolioComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/fixed-items/fixed-items.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fixed-items',
+  templateUrl: './fixed-items.component.html',
+  styleUrls: ['./fixed-items.component.scss']
+})
+export class FixedItemsComponent {
+
+}
