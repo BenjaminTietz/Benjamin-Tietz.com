05ce767056424cbaed28e27c9ac42c50c1477fb6
Benjamin Tietz <benjamin0tietz@gmail.com>
2023-10-14 22:15:07 +0200

    recent work on mobile version

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,5 @@
 import { Component, ElementRef } from '@angular/core';
-
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -8,9 +8,10 @@ import { Component, ElementRef } from '@angular/core';
 })
 export class HeaderComponent {
   isMenuOpen = false;
+  isNavUnderlined = false;
 
 
-  constructor(private elementRef: ElementRef) {}
+  constructor(private elementRef: ElementRef, private router: Router) {}
 
   toggleMenu() {
     const menu = this.elementRef.nativeElement.querySelector('#buger-menu');
@@ -22,4 +23,21 @@ export class HeaderComponent {
     }
   }
 
+  selectedElementId: string = ''; // Füge eine Variable hinzu, um das ausgewählte Element zu speichern
+
+  underlineNav(id: string) {
+    const clickedElement = this.elementRef.nativeElement.querySelector(`#${id}`);
+  
+    // Entferne die Klasse von allen Elementen
+    const allElements = this.elementRef.nativeElement.querySelectorAll('a');
+    allElements.forEach((element: HTMLElement) => {
+      element.classList.remove('purple-underline');
+    });
+  
+    // Füge die Klasse nur dem geklickten Element hinzu
+    if (clickedElement) {
+      clickedElement.classList.add('purple-underline');
+      this.selectedElementId = id; // Aktualisiere das ausgewählte Element
+    }
+  }
 }
